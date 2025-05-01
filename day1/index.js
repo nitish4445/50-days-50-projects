1141a7b1b771f8625c314098fbb64d0995d32bca
nitish4445 <nitishkumar09598@gmail.com>
2025-05-01 21:22:46 +0530

    01-05-2025

--- day1/index.js
+++ day1/index.js
@@ -1,13 +1,17 @@
-const panel=document.querySelectorAll(".panle");
-
-panel.forEach((panel)=>{
-    panel.addEventListener("click",()=>{
-        removeActiveClasses();
-        panel.classList.add("active");
+// let firstImg=document.querySelector(".firstImg");
+// firstImg.addEventListener("click",()=>{
+//     firstImg.classList.add("afterClick");
+// })
+let firstImg=document.querySelectorAll(".imageContainer");
+firstImg.forEach((e)=>{
+    e.addEventListener("click",()=>{
+        removeAfterClick();
+        e.classList.add("afterClick")
     })
+    
 })
-const removeActiveClasses=()=>{
-    panel.forEach((panel)=>{
-        panel.classList.remove('active')
+function removeAfterClick(){
+    firstImg.forEach((e)=>{
+        e.classList.remove("afterClick")
     })
 }
