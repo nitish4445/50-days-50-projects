d31c89d02f59be673356270bee6cc2c349769192
nitish4445 <nitishkumar09598@gmail.com>
2025-05-02 22:33:56 +0530

    some change remain

--- day1/index.js
+++ day1/index.js
@@ -2,11 +2,21 @@
 // firstImg.addEventListener("click",()=>{
 //     firstImg.classList.add("afterClick");
 // })
+let viewName=document.querySelectorAll(".viewName")
 let firstImg=document.querySelectorAll(".imageContainer");
 firstImg.forEach((e)=>{
     e.addEventListener("click",()=>{
         removeAfterClick();
-        e.classList.add("afterClick")
+        e.classList.add("afterClick");
+        let viewName = e.querySelector(".viewName");
+        document.querySelectorAll(".viewName").forEach((v) => {
+            v.style.display = "none";
+        });
+
+        // Show the h1 inside the clicked element
+        if (viewName) {
+            viewName.style.display = "block";
+        }
     })
     
 })
